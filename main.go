b9c3b12dc76abb3893dd4c32ad5fb3ab7404741a
Tim Millard <tim@rapidpaylegal.com>
2023-01-13 14:56:38 +1100

    Flag n for number of UUID's to generate

--- main.go
+++ main.go
@@ -16,31 +16,34 @@ func main() {
 	v5 := flag.Bool("v5", false, "generate version 5")
 	v6 := flag.Bool("v6", false, "generate version 6")
 	v7 := flag.Bool("v7", false, "generate version 7")
+	n := flag.Int("n", 1, "number of UUID's to generate")
 	namespace := flag.String("namespace", "", "namespace used for version 3 & 4 uuid's")
 	name := flag.String("name", "", "name used for version 3 & 4 uuid's")
 
 	flag.Parse()
 
-	var id uuid.UUID
-	switch {
-	case *zero:
-		id = uuid.Nil
-	case *v1:
-		id = uuid.Must(uuid.NewV1())
-	case *v3:
-		ns := uuid.FromStringOrNil(*namespace)
-		id = uuid.NewV3(ns, *name)
-	case *v4:
-		id = uuid.Must(uuid.NewV4())
-	case *v5:
-		ns := uuid.FromStringOrNil(*namespace)
-		id = uuid.NewV5(ns, *name)
-	case *v6:
-		id = uuid.Must(uuid.NewV6())
-	case *v7:
-		id = uuid.Must(uuid.NewV7())
-	default:
-		id = uuid.Must(uuid.NewV4())
+	for i := 0; i < *n; i++ {
+		var id uuid.UUID
+		switch {
+		case *zero:
+			id = uuid.Nil
+		case *v1:
+			id = uuid.Must(uuid.NewV1())
+		case *v3:
+			ns := uuid.FromStringOrNil(*namespace)
+			id = uuid.NewV3(ns, *name)
+		case *v4:
+			id = uuid.Must(uuid.NewV4())
+		case *v5:
+			ns := uuid.FromStringOrNil(*namespace)
+			id = uuid.NewV5(ns, *name)
+		case *v6:
+			id = uuid.Must(uuid.NewV6())
+		case *v7:
+			id = uuid.Must(uuid.NewV7())
+		default:
+			id = uuid.Must(uuid.NewV4())
+		}
+		fmt.Println(id)
 	}
-	fmt.Println(id)
 }
